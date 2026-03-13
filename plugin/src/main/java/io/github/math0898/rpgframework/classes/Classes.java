4e232ed17dca3fcf786b5e738176170e26498f5e
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2026-03-13 10:07:41 -0500

    Refactor Classes enum for improved structure

--- plugin/src/main/java/io/github/math0898/rpgframework/classes/Classes.java
+++ plugin/src/main/java/io/github/math0898/rpgframework/classes/Classes.java
@@ -2,93 +2,55 @@ package io.github.math0898.rpgframework.classes;
 
 import org.bukkit.ChatColor;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
- * The Classes class is an enum list of the classes currently implemented, and planned.
- *
- * @author Sugaku
+ * Enumerates the currently implemented and planned RPG classes.
  */
-public enum Classes { // TODO: Plan GLADIATOR,
+public enum Classes { // TODO: Plan GLADIATOR
 
-    /**
-     * The assassin is a mobile melee dps.
-     */
-    ASSASSIN(ChatColor.BLUE + "Assassin"),
+    ASSASSIN(ChatColor.BLUE, "Assassin"),
+    BARD(ChatColor.LIGHT_PURPLE, "Bard"),
+    BERSERKER(ChatColor.RED, "Berserker"),
+    PALADIN(ChatColor.YELLOW, "Paladin"),
+    PYROMANCER(ChatColor.GOLD, "Pyromancer"),
+    // MARKSMEN(ChatColor.DARK_GREEN, "Marksmen"), // Requires an implementing class.
+    NONE(ChatColor.WHITE, "None");
 
-    /**
-     * The bard is an aura casting support class.
-     */
-    BARD(ChatColor.LIGHT_PURPLE + "Bard"),
+    private static final Map<String, Classes> BY_NAME = Arrays.stream(values())
+            .collect(Collectors.toMap(
+                    classType -> classType.name(),
+                    Function.identity()
+            ));
 
-    /**
-     * The berserker class is a melee dps tank hybrid.
-     */
-    BERSERKER(ChatColor.RED + "Berserker"),
+    private final String displayName;
+    private final String formattedName;
 
-    /**
-     * A healer, and a tank, the paladin is a powerful front-liner.
-     */
-    PALADIN(ChatColor.YELLOW + "Paladin"),
-
-    /**
-     * The pyromancer is a master of fire magics.
-     */
-    PYROMANCER(ChatColor.GOLD + "Pyromancer"),
-
-    /**
-     * The marksmen class is a class of powerful archers.
-     */
-    //MARKSMEN(ChatColor.DARK_GREEN + "Marksmen"), // todo: Requires an implementing class.
-
-    /**
-     * The none class represents both 'class missing' and 'no class assigned'.
-     */
-    NONE(ChatColor.WHITE + "None");
-
-    /**
-     * The formatted string version of this enum value.
-     */
-    private final String formatted;
-
-    /**
-     * Creates a new Classes enum entry.
-     *
-     * @param formatted The formatted string version of this enum value.
-     */
-    Classes (String formatted) {
-        this.formatted = formatted;
+    Classes(ChatColor color, String displayName) {
+        this.displayName = displayName;
+        this.formattedName = color + displayName;
     }
 
-    /**
-     * An accessor method to the formatted text version of this class.
-     *
-     * @return The formatted name of this class.
-     */
-    public String getFormattedName () {
-        return  formatted;
+    public String getFormattedName() {
+        return formattedName;
     }
 
-    /**
-     * An accessor method for a text version of this class.
-     *
-     * @return The name of this class without any color formatting.
-     */
-    public String getName () {
-        char[] str = this.toString().toLowerCase().toCharArray();
-        str[0] = Character.toUpperCase(str[0]);
-        return new String(str);
+    public String getName() {
+        return displayName;
     }
 
-    /**
-     * Checks for the correct Classes enum value based on the given string.
-     *
-     * @param string The string to convert to a Classes enum value.
-     * @return Either the class named in the string, or {@link Classes#NONE}.
-     */
-    public static Classes fromString (String string) {
-        Classes[] values = Classes.values();
-        for (Classes c : values)
-            if (string.equalsIgnoreCase(c.toString()))
-                return c;
-        return Classes.NONE;
+    public static Classes fromString(String value) {
+        if (value == null || value.isBlank()) {
+            return NONE;
+        }
+
+        return BY_NAME.getOrDefault(normalize(value), NONE);
+    }
+
+    private static String normalize(String value) {
+        return value.trim().toUpperCase();
     }
 }
